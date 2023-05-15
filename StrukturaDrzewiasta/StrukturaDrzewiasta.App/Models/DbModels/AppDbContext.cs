0a6297f96a2d00200d4995f46d28d4f58f983b8c
QuSZo <mikolajkuszowski@gmail.com>
2023-05-15 21:54:55 +0200

    Entity added and primary configuration of AppDbContext done

---
+++ StrukturaDrzewiasta/StrukturaDrzewiasta.App/Models/DbModels/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StrukturaDrzewiasta.App.Models.DbModels;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Node> Node { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseSerialColumns();
+    }
+}
---
+++ StrukturaDrzewiasta/StrukturaDrzewiasta.App/Models/DbModels/Node.cs
@@ -0,0 +1,8 @@
+﻿namespace StrukturaDrzewiasta.App.Models.DbModels;
+
+public class Node
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Node> Nodes { get; set; }
+}
